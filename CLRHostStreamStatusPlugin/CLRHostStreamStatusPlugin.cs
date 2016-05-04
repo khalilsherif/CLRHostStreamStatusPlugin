f896f746d5bc47469998ae103fcdb766300462ae
Khalil Sherif <sherif.khalil@live.ca>
2016-05-04 17:14:56 -0400

    Initial commit

---
+++ CLRHostStreamStatusPlugin/CLRHostStreamStatusPlugin.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.IO.Pipes;
+
+using CLROBS;
+
+namespace CLRHostStreamStatusPlugin
+{
+    public class CLRHostStreamStatusPlugin : Plugin
+    {
+        private string _pipeName = "OBSStreamStatusPlugin_OpenBot";
+        public string Description
+        {
+            get
+            {
+                return "Provides events for stream start and stop to OpenBot plugins";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "OpenBot Stream Status IPC";
+            }
+        }
+
+        public bool LoadPlugin()
+        {
+            return true;
+        }
+
+        public void OnStartStream()
+        {
+            SendStreamStatus(true);
+        }
+
+        public void OnStopStream()
+        {
+            SendStreamStatus(false);
+        }
+
+        private void SendStreamStatus(bool online)
+        {
+            NamedPipeClientStream client = new NamedPipeClientStream(".", _pipeName, PipeDirection.Out);
+            client.Connect(1000);
+            if (client.IsConnected)
+            {
+                client.Write(online ? new byte[] { 1 } : new byte[] { 0 }, 0, 1);
+                client.Close();
+                client.Dispose();
+            }
+        }
+        public void UnloadPlugin()
+        {
+            SendStreamStatus(false);
+        }
+    }
+}
